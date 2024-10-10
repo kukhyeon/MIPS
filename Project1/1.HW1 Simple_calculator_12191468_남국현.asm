886ca2d4e86d13918b71da11573db399466a07f7
DESKTOP-135RQ3N\namkukhyeon <ska5686a@gmail.com>
2024-10-11 00:18:45 +0900

    성공한 버전. 대부분의 경우의 수가 되는 것 같다. 음수 처리만 하면 완성될 것 같다. 1. 첫 번째 피연산자의 음수 처리 2. 두 번째 피연산자의 음수 처리 3. 음수 처리 후의 연산 -> MIPS가 음수 값에 대해서도 정상적으로 연산을 수행하므로, 손 볼 부분은 없겠다.

---
+++ Project1/1.HW1 Simple_calculator_12191468_남국현.asm
@@ -0,0 +1,133 @@
+###################################
+#  HW1: Simple string calculator  #
+###################################
+#
+#  Data segment
+#
+		.data
+input:		.asciiz  "Enter input (e.g. 1+2): "	# accept input expression
+error:		.asciiz  "Input error!" 
+sol:    	.asciiz  "Answer = "	# label for "Anmswer: " 
+plus: 		.asciiz  "+"		# label for "+"
+minus:		.asciiz  "-"		# label for "-"
+multiple:   .asciiz  "*"    # 곱셈 기호
+divider:    .asciiz  "/"    # 나눗셈 기호
+div_error:	.asciiz	 "0으로 나눌 수 없습니다." # 예외 추가
+exp:		.word 	 0:15   	# define buffer for input string
+size: 		.word  	 15			# size of buffer
+
+#
+#  Text segment
+#
+		.text
+main:	la 	$a0,input  		# print 
+		li 	$v0,4			# "Enter input (e.g.):"
+		syscall
+		la 	$a0,exp			# load Buffer for input string         
+		la  $a1,size		# load size to $a1  
+		li  $v0,8 	    	# read string
+		syscall
+		la	$t0, exp		# exp의 base address load
+		jal load_input		# load operand1, 2, operator
+		jal	operator		# decide operation
+		jal	print			# print result
+		
+_end:	li 	$v0,10      	# system call for exit       
+        syscall   			# EXIT!
+		
+#
+#  Subroutine to load operands & operator, and to print the result 
+#
+
+	
+load_input: addi 	$sp, $sp, -4	# 스택에 공간 확보 
+			sw 		$ra, 0($sp)		# $ra 값을 스택에 저장
+			li      $s0, 0          # 첫 번째 피연산자 초기화
+        	li      $s1, 0          # 두 번째 피연산자 초기화
+			jal 	op1
+			lb 		$t2,0($t0)		# load operator to $t2
+			addi	$t0, $t0, 1		# 다음 주소로          
+			jal 	op2
+
+			lw 		$ra, 0($sp)		# 스택에서 $ra 값 복원
+			addi 	$sp, $sp, 4		# 스택 포인터 복원
+			jr   	$ra				# return from subroutine    
+
+op1:		lb		$t1,0($t0)     	# 현재 문자 load
+			beq     $t1, '\n', end_op   # 개행 문자 처리
+			beq     $t1, '\0', end_op   # 널 종결자 처리
+			li		$t3, '0'		# ASCII '0' = 48	
+			li		$t4, '9'		# ASCII	'9' = 57
+			# $t1 > 57 && $t1 < 48이면 숫자가 아니다.
+			blt     $t1, $t3, end_op
+    		bgt     $t1, $t4, end_op
+			
+			# 숫자일 경우
+			addi 	$t1, $t1, -48 	# ASCII -> 숫자 변환
+			mul		$s0, $s0, 10	# 자리수 증가
+			add     $s0, $s0, $t1	# 현재 숫자 더함
+			addi	$t0, $t0, 1		# 다음 문자로 이동
+			j 		op1
+
+op2:		lb		$t1,0($t0)     	# 현재 문자 load
+			beq     $t1, '\n', end_op   # 개행 문자 처리
+			beq     $t1, '\0', end_op   # 널 종결자 처리
+			li		$t3, '0'		# ASCII '0' = 48	
+			li		$t4, '9'		# ASCII	'9' = 57
+			# $t1 > 57 && $t1 < 48이면 숫자가 아니다.
+			blt     $t1, $t3, error
+    		bgt     $t1, $t4, error
+			
+			# 숫자일 경우
+			addi 	$t1, $t1, -48 	# ASCII -> 숫자 변환
+			mul		$s1, $s1, 10	# 자리수 증가
+			add     $s1, $s1, $t1	# 현재 숫자 더함
+			addi	$t0, $t0, 1		# 다음 문자로 이동		
+			j 		op2
+
+end_op: 	jr $ra
+
+operator:	lb 		$t5, plus		# load "+" to $t5
+			lb		$t6, minus		# load "-" to $t5
+			lb      $t7, multiple    # '*' 로드
+			lb      $t8, divider     # '/' 로드
+
+			beq     $t2, $t5, add_op    # '+'이면 add_op로 분기
+			beq     $t2, $t6, sub_op    # '-'이면 sub_op로 분기
+			beq     $t2, $t7, mul_op    # '*'이면 mul_op로 분기
+			beq     $t2, $t8, div_op    # '/'이면 div_op로 분기
+
+			la      $a0, error          # 그 외에는 오류 메시지 출력
+			li      $v0, 4
+			syscall
+			b       _end
+
+add_op:   	add 	$s2,$s0,$s1		# operand1 + operand2
+        	jr 		$ra				# return from subroutine 
+        	
+sub_op:  	sub 	$s2,$s0,$s1		# operand1 - operand2
+			jr 		$ra				# return from subroutine 
+
+
+mul_op:		mul     $s2, $s0, $s1       # operand1 * operand2
+			jr      $ra                 # 서브루틴 종료
+
+div_op:
+			beq     $s1, $zero, div_errorL   # 에러 레이블로 분기
+			div     $s0, $s1
+			mflo    $s2
+			jr      $ra
+
+div_errorL:
+			la      $a0, div_error      # "0으로 나눌 수 없습니다." 출력
+			li      $v0, 4
+			syscall
+			b       _end                # 프로그램 종료
+
+print: 		la 		$a0,sol    		# load "=" to $a0
+			li 		$v0,4			# print "="
+			syscall
+			move    $a0, $s2        # 결과 값을 $a0로 이동
+			li      $v0, 1          # 정수 출력 syscall
+			syscall
+			jr 		$ra				# return from subroutine 
